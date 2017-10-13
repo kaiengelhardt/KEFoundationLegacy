b51c3922cf6825a52cd0a0d70615cefb24bef16d
Kai Engelhardt <kai@kaiengelhardt.com>
2017-10-13 23:15:05 +0200

    Add conversion methods for CGSize and CGPoint

---
+++ KaiKit/Classes/Foundation/CGPoint+Conversions.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public extension CGPoint {
+	
+	public var asCGSize: CGSize {
+		return CGSize(width: x, height: y)
+	}
+	
+}
---
+++ KaiKit/Classes/Foundation/CGSize+Conversions.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public extension CGSize {
+	
+	public var asCGPoint: CGPoint {
+		return CGPoint(x: width, y: height)
+	}
+	
+}
