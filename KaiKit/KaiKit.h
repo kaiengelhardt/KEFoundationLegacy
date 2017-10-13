4a46583df68d7ab69f7ee03cbaf8dc62c4efe09a
Kai Engelhardt <kai@kaiengelhardt.com>
2017-10-13 20:57:40 +0200

    Initial commit

---
+++ KaiKit/KaiKit.h
@@ -0,0 +1,13 @@
+// Created by Kai Engelhardt on 13.10.17.
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KaiKit.
+FOUNDATION_EXPORT double KaiKitVersionNumber;
+
+//! Project version string for KaiKit.
+FOUNDATION_EXPORT const unsigned char KaiKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KaiKit/PublicHeader.h>
+
+
