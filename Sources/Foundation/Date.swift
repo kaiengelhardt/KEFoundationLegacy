5cd3c36a59da1b88388c4dc9d802643985f4fd46
Kai Engelhardt <kai@kaiengelhardt.com>
2018-01-31 22:50:26 +0100

    Add documentation to Date.normalized

--- Sources/Foundation/Date.swift
+++ Sources/Foundation/Date.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public extension Date {
 	
+	/// Returns a date which is set to 12am (0:00) in GMT+0.
 	public var normalized: Date {
 		var calendar = Calendar(identifier: .gregorian)
 		calendar.timeZone = TimeZone(secondsFromGMT: 0)!
