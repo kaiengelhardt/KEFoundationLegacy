7630845f3685cd95293f0b571e2e2a6cbb2baf51
Kai Engelhardt <kai@kaiengelhardt.com>
2018-01-31 18:03:09 +0100

    Add NotificationObserver

---
+++ Sources/Foundation/NotificationObserver.swift
@@ -0,0 +1,32 @@
+// Created by Kai Engelhardt on 31.01.18.
+// Based on https://twitter.com/jaredsinclair/status/951536021459619840
+
+import Foundation
+public class NotificationObserver {
+	
+	private var observers = [NSObjectProtocol]()
+	private let queue: OperationQueue
+	
+	public init(queue: OperationQueue = .main) {
+		self.queue = queue
+	}
+	
+	deinit {
+		for observer in observers {
+			NotificationCenter.default.removeObserver(observer)
+		}
+	}
+	
+	// MARK: - Action
+	/// Adds an entry to the notification center's dispatch table that includes a notification name and a block to add to the queue.
+	/// All observers will be removed when the `NotificationObserver` is released.
+	///
+	/// - Parameters:
+	///   - name: The notification name.
+	///   - handler: The block to be executed when the notification is received.
+	public func when(_ name: Notification.Name, perform handler: @escaping (Notification) -> Void) {
+		let observer = NotificationCenter.default.addObserver(forName: name, object: nil, queue: queue, using: handler)
+		observers.append(observer)
+	}
+	
+}
