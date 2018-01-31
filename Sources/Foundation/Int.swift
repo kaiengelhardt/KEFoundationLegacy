3765ffb17b3283d6dee78dd4532e8ed208370e3b
Kai Engelhardt <kai@kaiengelhardt.com>
2018-01-31 20:12:43 +0100

    Add documentation

--- Sources/Foundation/Int.swift
+++ Sources/Foundation/Int.swift
@@ -2,6 +2,15 @@ import Foundation
 
 public extension Int {
 	
+	/// Generates a random number between lowerBound and upperBound.
+	///
+	/// - returns:
+	///   A random number between lowerBound and upperBound.
+	///
+	/// - parameters:
+	///   - lowerBound: The minimum value (inclusive).
+	///   - upperBound: The maximum value (inclusive).
+	///
 	public static func randomNumber(between lowerBound: Int, and upperBound: Int) -> Int {
 		return Int(arc4random_uniform(UInt32(upperBound - lowerBound))) + lowerBound
 	}
