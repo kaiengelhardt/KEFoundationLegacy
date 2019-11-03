decf02efce7ca1ab78e5370ec26b8c065ca7ebcc
Kai Engelhardt <kai@kaiengelhardt.com>
2019-11-03 21:46:48 +0100

    Temporarily disable animations. This function needs to be worked on.

--- Sources/Core Data/UITableView+FetchedResultsUpdater.swift
+++ Sources/Core Data/UITableView+FetchedResultsUpdater.swift
@@ -44,29 +44,30 @@ extension UITableView: FetchedResultsUpdaterDelegate {
 	}
 	
 	public func updater(_ updater: FetchedResultsUpdater, didUpdateWithSectionUpdates sectionUpdates: [FetchedResultsUpdater.SectionUpdate], rowUpdates: [FetchedResultsUpdater.RowUpdate]) {
-		beginUpdates()
-		for update in sectionUpdates {
-			switch update {
-			case .insert(let sectionIndex):
-				insertSections(IndexSet(integer: sectionIndex), with: .automatic)
-			case .delete(let sectionIndex):
-				deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
-			}
-		}
-
-		for update in rowUpdates {
-			switch update {
-			case .insert(let indexPath):
-				insertRows(at: [indexPath], with: .automatic)
-			case .delete(let indexPath):
-				deleteRows(at: [indexPath], with: .automatic)
-			case .update(let indexPath):
-				reloadRows(at: [indexPath], with: .automatic)
-			case .move(let (fromIndexPath, toIndexPath)):
-				reloadRows(at: [fromIndexPath, toIndexPath], with: .automatic)
-			}
-		}
-		endUpdates()
+		reloadData()
+//		beginUpdates()
+//		for update in sectionUpdates {
+//			switch update {
+//			case .insert(let sectionIndex):
+//				insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+//			case .delete(let sectionIndex):
+//				deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+//			}
+//		}
+//
+//		for update in rowUpdates {
+//			switch update {
+//			case .insert(let indexPath):
+//				insertRows(at: [indexPath], with: .automatic)
+//			case .delete(let indexPath):
+//				deleteRows(at: [indexPath], with: .automatic)
+//			case .update(let indexPath):
+//				reloadRows(at: [indexPath], with: .automatic)
+//			case .move(let (fromIndexPath, toIndexPath)):
+//				reloadRows(at: [fromIndexPath, toIndexPath], with: .automatic)
+//			}
+//		}
+//		endUpdates()
 	}
 	
 }
